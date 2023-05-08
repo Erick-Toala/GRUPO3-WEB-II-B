1407b39f1010531994595e0b3111323c8ffb4de3
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-07 21:44:19 -0500

    Update cancion.controller.ts

--- src/cancion/cancion.controller.ts
+++ src/cancion/cancion.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 import { json } from 'stream/consumers';
+
 import {CreateCancionDTO} from './dto/cancion.dto';
 import { CancionService } from './cancion.service';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Canción')
+@Controller('cancion')
 export class CancionController {
+  
   constructor(private cancionService:CancionService){}
     
     @Post('/create')
@@ -24,5 +28,41 @@ export class CancionController {
             canciones
         })
     }
+  
+    @ApiParam({
+        name:'cancionID'
+    })
+    @Get('/:cancionID')
+    async getCancion(@Res() res, @Param('cancionID') cancionID){
+        const cancion = await this.cancionService.getCancion(cancionID);
+        if(!cancion)throw new NotFoundException('La canción no existe');
+        return res.status(HttpStatus.OK).json(cancion);
+    }
 
+    @ApiParam({
+        name:'cancionID'
+    })
+    @Delete('/delete/:cancionID')
+    async deleteCancion(@Res() res, @Param('cancionID') cancionID){
+        const cancionDeleted = await this.cancionService.deleteCancion(cancionID);
+        if(!cancionDeleted)throw new NotFoundException('La canción no existe');
+        return res.status(HttpStatus.OK).json({
+            message:'Canción eliminada con éxito!',
+            cancionDeleted
+        });
+    }
+
+    @ApiParam({
+        name:'cancionID'
+    })
+  
+    @Put('/update/:cancionID')
+    async updateCancion(@Res() res, @Body() createCancionDTO:CreateCancionDTO, @Param('cancionID')cancionID){
+        const cancionUpdate = await this.cancionService.updateCancion(cancionID, createCancionDTO);
+        if(!cancionUpdate)throw new NotFoundException('La canción no existe');
+        return res.status(HttpStatus.OK).json({
+            message:'Canción actualizada con éxito!',
+            cancionUpdate
+        });
+    }
 }
