b7505037c5889d0f30a53b607ff85316559e5a1f
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-07 21:46:54 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { AutorModule } from './autor/autor.module';
+import { AlbumModule } from './album/album.module';
+
 @Module({
+  imports: [CancionModule, MongooseModule.forRoot('mongodb+srv://toalaerick56:toalaerick56@cluster0.9flgjye.mongodb.net/music-repertorio-80s'), AutorModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
