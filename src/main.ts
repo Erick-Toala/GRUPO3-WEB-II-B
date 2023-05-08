a4ea4233de5d197364a3ea7c23b52d92dba2d3cb
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-07 21:45:53 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('MUSIC API')
+    .setDescription('Aplicación de repertorio para música de los 80s')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(5000);
 }
 bootstrap();
