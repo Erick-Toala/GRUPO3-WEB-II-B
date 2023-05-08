8032defee75fc1fa88932e9eaf9d9d5b9ea46bb3
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-05-07 21:24:40 -0500

    Update cancion.service.ts

--- src/cancion/cancion.service.ts
+++ src/cancion/cancion.service.ts
@@ -5,4 +5,15 @@ import { Cancion } from './interfaces/cancion.interface';
 import { CreateCancionDTO } from './dto/cancion.dto';
 @Injectable()
 export class CancionService {
+  constructor(@InjectModel('Cancion') private readonly cancionModel:Model<Cancion>){}
+
+    async getCanciones(): Promise<Cancion[]> {
+        const canciones = await this.cancionModel.find().populate('idArtista', 'nombre').populate('idAlbum', 'nombre').exec();
+        return canciones;
+    }
+
+    async getCancion(cancionID:string):Promise<Cancion>{
+        const cancion = await this.cancionModel.findById(cancionID).populate('idArtista', 'nombre').populate('idAlbum', 'nombre').exec();;
+        return cancion;
+    }
 }
