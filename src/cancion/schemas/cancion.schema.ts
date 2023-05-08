02846ccef14130d8ac91356ead2506796626e40c
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-05-07 21:40:22 -0500

    Update cancion.schema.ts

--- src/cancion/schemas/cancion.schema.ts
+++ src/cancion/schemas/cancion.schema.ts
@@ -1,2 +1,16 @@
 import { Schema } from 'mongoose';
-
+export const CancionSchema = new Schema({
+    nombre:{
+        type:String,
+        required:true
+    },
+    duracion:String,
+    idArtista:{type: Schema.Types.ObjectId, ref:"Autor"},
+    idAlbum:{type: Schema.Types.ObjectId, ref:"Album"},
+    urlPortada:String,
+    urlYouTube:String,
+    fechaLanzamiento:{
+        type:Date,
+        default:Date.now
+    }
+});
