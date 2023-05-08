314eb27ddc12e28ca253eb34e6be44962b7f7d5c
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-05-07 21:21:03 -0500

    Update cancion.service.ts

--- src/cancion/cancion.service.ts
+++ src/cancion/cancion.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cancion } from './interfaces/cancion.interface';
+import { CreateCancionDTO } from './dto/cancion.dto';
 @Injectable()
 export class CancionService {
 }
