4eb1be2151f04d1477cfa957071330a993593753
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-05-07 21:37:24 -0500

    Update cancion.dto.ts

--- src/cancion/dto/cancion.dto.ts
+++ src/cancion/dto/cancion.dto.ts
@@ -1,2 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
+export class CreateCancionDTO{
 
+    @ApiProperty()readonly nombre:string;
+    @ApiProperty()readonly duracion:string;
+    @ApiProperty()readonly idArtista:string;
+    @ApiProperty()readonly idAlbum:string;
+    @ApiProperty()readonly urlPortada:string;
+    @ApiProperty()readonly urlYouTube:string;
+    @ApiProperty()readonly fechaLanzamiento:Date;
+}
