6ad5158886d4906d338a18d7fd7e82f5d5e313c2
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-09 13:46:30 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//importamos los modulos necesarios
 import { MongooseModule } from '@nestjs/mongoose';
 import { AutorModule } from './autor/autor.module';
 import { AlbumModule } from './album/album.module';
