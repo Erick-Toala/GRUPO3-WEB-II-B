cd7f8d7cf112b27592de3a5aafe00fdf5a4fe40d
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-09 12:32:42 -0500

    Update album.dto.ts

--- src/album/dto/album.dto.ts
+++ src/album/dto/album.dto.ts
@@ -1,3 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { CreateCancionDTO } from "src/cancion/dto/cancion.dto";
 
+export class CreateAlbumDTO{
+    @ApiProperty()readonly nombre:string;
+    @ApiProperty()readonly disquera:string;
+    @ApiProperty()readonly numCanciones:number;
+    @ApiProperty()readonly cancionPopular:string;
+    @ApiProperty()readonly idArtista:string;
+    @ApiProperty()readonly idCanciones:Array<CreateCancionDTO>;
+    @ApiProperty()readonly urlAlbum:string;
+    @ApiProperty()readonly fechaLanzamiento:Date;
+}
