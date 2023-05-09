48a47f829782fd2cf38a25af33869ebd45b4b612
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-09 12:33:22 -0500

    Update album.interface.ts

--- src/album/interfaces/album.interface.ts
+++ src/album/interfaces/album.interface.ts
@@ -1,3 +1,13 @@
 import {Document} from 'mongoose';
 import { CreateCancionDTO } from 'src/cancion/dto/cancion.dto';
 
+export interface Album extends Document{
+    readonly nombre:string;
+    readonly disquera:string;
+    readonly numCanciones:number;
+    readonly cancionPopular:string;
+    readonly idArtista:string;
+    readonly idCanciones:Array<CreateCancionDTO>;
+    readonly urlAlbum:string;
+    readonly fechaLanzamiento:Date;
+}
