e42b4897ef402b394d8fc968bcabee737a265256
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-09 13:46:18 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AutorModule } from './autor/autor.module';
 import { AlbumModule } from './album/album.module';
 
 @Module({
+  //el modulo de mongoose usa un metodo (forRoot), que recibe un parametro (cadena de conexion de mongodb) 
   imports: [CancionModule, MongooseModule.forRoot('mongodb+srv://toalaerick56:toalaerick56@cluster0.9flgjye.mongodb.net/music-repertorio-80s'), AutorModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
