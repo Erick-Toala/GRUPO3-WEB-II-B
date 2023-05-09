92815c24ae5323a50bb32fddd901d50d0a36e9c1
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-09 13:49:15 -0500

    Update cancion.service.ts

--- src/cancion/cancion.service.ts
+++ src/cancion/cancion.service.ts
@@ -7,13 +7,17 @@ import { CreateCancionDTO } from './dto/cancion.dto';
 
 @Injectable()
 export class CancionService {
+
+  //llamamos al constructor para injectar el modelo Cancion dentro de la clase
   constructor(@InjectModel('Cancion') private readonly cancionModel:Model<Cancion>){}
 
+    //Método GET para obtener datos de todas las canciones guardadas en mongodb
     async getCanciones(): Promise<Cancion[]> {
         const canciones = await this.cancionModel.find().populate('idArtista', 'nombre').populate('idAlbum', 'nombre').exec();
         return canciones;
     }
 
+    //Método GET para obtener datos de una canción por su ID guardada en mongodb
     async getCancion(cancionID:string):Promise<Cancion>{
         const cancion = await this.cancionModel.findById(cancionID).populate('idArtista', 'nombre').populate('idAlbum', 'nombre').exec();;
         return cancion;
