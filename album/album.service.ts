c28914569892a873f2b094b4e625b0381c7a9df9
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-08 20:02:18 -0500

    Delete album.service.ts

--- album/album.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
-import { Album } from './interfaces/album.interface';
-import { CreateAlbumDTO } from './dto/album.dto';
-
-@Injectable()
