8c25f7282fdf10d2d736d2ef2750df063b57897d
JOAN2021g <joangallardo1518@gmail.com>
2023-05-11 00:27:53 -0500

    update autor.module.ts

--- src/autor/autor.module.ts
+++ src/autor/autor.module.ts
@@ -5,3 +5,13 @@ import { AutorService } from './autor.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import {AutorSchema} from './schemas/autor.schema';
 
+@Module({
+  imports:[
+    MongooseModule.forFeature([
+      {name:'Autor',schema:AutorSchema}
+    ])
+  ],
+  controllers: [AutorController],
+  providers: [AutorService]
+})
+export class AutorModule {}
