02e17f17dd9c0c9a20b48654f8c5836bb02e43e9
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-05-11 15:36:12 -0500

    Update cancion.dto.ts

--- src/cancion/dto/cancion.dto.ts
+++ src/cancion/dto/cancion.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
+
+//Data transfer object
+//Se realiza la creación de la clase y se le agrega "export" para enviar los datos de tal forma que sea Servidor--Cliente
 export class CreateCancionDTO{
 
+    //definimos las propiedades de la entidad que se van a recibir del cliente, @ApiProperty() se usa para el Swagger
+    //Para que las propiedades de la clase sean visibles para el SwaggerModule, tenemos que anotarlas con el decorador @ApiProperty()
     @ApiProperty()readonly nombre:string;
     @ApiProperty()readonly duracion:string;
     @ApiProperty()readonly idArtista:string;
