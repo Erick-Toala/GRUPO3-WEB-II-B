d254c7aa832d721833e8aca49772a14adc62c7bc
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-10 22:00:38 -0500

    Update album.schema.ts

--- src/album/schemas/album.schema.ts
+++ src/album/schemas/album.schema.ts
@@ -1,7 +1,9 @@
+//importamos Schema desde mongoose
 import { Schema } from 'mongoose';
 
-
+//modelamos los datos que vamos a guardar dentro de mongoose en un Schema
 export const AlbumSchema = new Schema({
+    //definimos todas las propiedades que vamos a guardar en mongodb
     nombre:{
         type:String,
         required:true
