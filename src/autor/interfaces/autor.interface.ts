9055d7402eed527201e473c04e3ddda4b64256e9
JOAN2021g <joangallardo1518@gmail.com>
2023-05-11 00:45:27 -0500

    update autor.schemas.ts

--- src/autor/interfaces/autor.interface.ts
+++ src/autor/interfaces/autor.interface.ts
@@ -5,7 +5,7 @@ import {Document} from 'mongoose';
 //definimos una interface para detallar que es lo que vamos a estar manejando dentro del codigo
 export interface Autor extends Document{
 
-    //propiedades
+   //definimos las propiedades
     readonly nombre:string;
     readonly nacionalidad:string;
     readonly fechaNacimiento:Date;
--- src/autor/schemas/autor.schema.ts
+++ src/autor/schemas/autor.schema.ts
@@ -1,6 +1,11 @@
+//importamos Schema desde mongoose
 import { Schema } from 'mongoose';
 
+//modelamos los datos que vamos a guardar dentro de mongoose en un Schema
 export const AutorSchema = new Schema({
+    
+    //definimos todas las propiedades que vamos a guardar en mongodb
+  
     nombre:{
         type:String,
         required:true
