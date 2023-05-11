d79c07dbe21024abc30582c15a69e8c8be5b9168
JOAN2021g <joangallardo1518@gmail.com>
2023-05-11 00:34:36 -0500

    update autor.controller.ts

--- src/autor/autor.controller.ts
+++ src/autor/autor.controller.ts
@@ -1,23 +1,33 @@
 import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
-
 import {CreateAutorDTO} from './dto/autor.dto';
-
 import {AutorService} from './autor.service';
-
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 
 
-
-@ApiTags('artista')
+@ApiTags('Artista')
 
 @Controller('autor')
 
 export class AutorController {
 
-    constructor(private autorService: AutorService){
-
-
+    constructor(private autorService: AutorService){}
+    @Post('/create')
+    async createPost(@Res() res, @Body() createAutorDTO:CreateAutorDTO){
+        const autor = await this.autorService.createAutor(createAutorDTO);
+        return res.status(HttpStatus.OK).json({
+            message: 'Autor guardado con éxito!',
+            autor
+        })
     }
 
+    @Get('/')
+    async getAutores(@Res() res){
+        const autores = await this.autorService.getAutores();
+        res.status(HttpStatus.OK).json({
+            autores
+        })
+    }
+
+    
     
 }
