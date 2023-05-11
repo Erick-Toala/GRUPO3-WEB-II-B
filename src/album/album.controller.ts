9d30b9c6e630c5df4df69a200479017fc55e1c51
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-10 22:04:04 -0500

    Update album.controller.ts

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,16 +1,18 @@
+//Importación de librerías, RUTAS, decoradores, incorporando interfaces y clases
 import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 
 import {CreateAlbumDTO} from './dto/album.dto';
 import { AlbumService } from './album.service';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
-
+//adjuntamos un controlador a una etiqueta específica (Álbum) con el use el decorador @ApiTags
 @ApiTags('Álbum')
 @Controller('album')
 
 export class AlbumController {
-
+    //instanciamos la clase AlbumService en el constructor
     constructor(private albumService:AlbumService){}
 
+    //creamos un ruta a traves del metodo POST que nos permita insertar nuevos albumes
     @Post('/create')
     async createPost(@Res() res, @Body() createAlbumDTO:CreateAlbumDTO){
         const album = await this.albumService.createAlbum(createAlbumDTO);
@@ -20,7 +22,7 @@ export class AlbumController {
             album: album
         });
     }
-
+    //creamos un ruta a traves del metodo GET que nos permita obtener todos albumes
     @Get('/')
     async getAlbumes(@Res() res){
         const albumes = await this.albumService.getAlbumes();
@@ -28,7 +30,7 @@ export class AlbumController {
             albumes
         })
     }
-
+    //creamos un ruta a traves del metodo GET que nos permita obtener un album por su ID
     @ApiParam({
         name:'albumID'
     })
@@ -36,9 +38,10 @@ export class AlbumController {
     async getAlbum(@Res() res, @Param('albumID') albumID){
         const album = await this.albumService.getAlbum(albumID);
         if(!album)throw new NotFoundException('La álbum no existe');
+        //si no recibo/encuentra ningun album retornamos un error con el NotFoundException
         return res.status(HttpStatus.OK).json(album);
     }
-
+    //creamos un ruta a traves del metodo DELETE que nos permita eliminar un album por su ID
     @ApiParam({
         name:'albumID'
     })
@@ -46,12 +49,13 @@ export class AlbumController {
     async deleteAlbum(@Res() res, @Query('albumID') albumID){
         const albumDeleted = await this.albumService.deleteAlbum(albumID);
         if(!albumDeleted)throw new NotFoundException('El álbum  no existe');
+        //si no recibo/encuentra ningun album retornamos un error con el NotFoundException
         return res.status(HttpStatus.OK).json({
             message:'Álbum eliminada con éxito!',
             albumDeleted
         });
     }
-
+    //creamos un ruta a traves del metodo PUT que nos permita actualizar un album por su ID
     @ApiParam({
         name:'albumID'
     })
@@ -59,6 +63,7 @@ export class AlbumController {
     async updateAlbum(@Res() res, @Body() createAlbumDTO:CreateAlbumDTO, @Query('albumID')albumID){
         const albumUpdate = await this.albumService.updateAlbum(albumID, createAlbumDTO);
         if(!albumUpdate)throw new NotFoundException('El álbum  no existe');
+        //si no recibo/encuentra ningun album retornamos un error con el NotFoundException
         return res.status(HttpStatus.OK).json({
             message:'Álbum actualizada con éxito!',
             albumUpdate
