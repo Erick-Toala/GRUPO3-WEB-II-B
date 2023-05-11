7dc56b4ea7af0965eea5ec7cc0d855d8877f2b25
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-05-11 15:41:49 -0500

    Update cancion.schema.ts

--- src/cancion/schemas/cancion.schema.ts
+++ src/cancion/schemas/cancion.schema.ts
@@ -1,5 +1,9 @@
+//importamos Schema desde mongoose
 import { Schema } from 'mongoose';
+//modelamos los datos que vamos a guardar dentro de mongoose en un Schema
+//lo guardamos en una constante para exportar el Schema
 export const CancionSchema = new Schema({
+ //definimos todas las propiedades que vamos a guardar en mongodb
     nombre:{
         type:String,
         required:true
