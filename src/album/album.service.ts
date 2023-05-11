c983a523a0d508fe1631ae1b5dbc1bc1e1193662
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-10 22:05:53 -0500

    Update album.service.ts

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,3 +1,4 @@
+//Importación de librerías, modelos, modulos incorporando interfaces y clases
 import { Injectable } from '@nestjs/common';
 
 import { Model } from 'mongoose';
@@ -7,29 +8,30 @@ import { CreateAlbumDTO } from './dto/album.dto';
 
 @Injectable()
 export class AlbumService {
-
+    //llamamos al constructor para injectar el modelo Album dentro de la clase
     constructor(@InjectModel('Album') private readonly albumModel:Model<Album>){}
-
+        
+    //Método GET para obtener datos de todos las albumes guardadas en mongodb
     async getAlbumes(): Promise<Album[]> {
         const albumes = await this.albumModel.find().populate('cancionPopular idArtista', 'nombre').populate('idCanciones','nombre').exec();
         return albumes;
     }
-
+    //Método GET para obtener datos de un album por su ID guardada en mongodb
     async getAlbum(albumID:string):Promise<Album>{
         const album = await this.albumModel.findById(albumID).populate('idArtista', 'nombre').exec();;
         return album;
     }
-
+    //Método CREATE para crear un nuevo album en mongodb
     async createAlbum(createAlbumDTO:CreateAlbumDTO):Promise<Album>{
         const album = new this.albumModel(createAlbumDTO);
         return await album.save();
     }
-
+    //Método DELETE para eliminar un album por su ID de mongodb
     async deleteAlbum(albumID:string):Promise<Album>{
         const deleteAlbum = await this.albumModel.findByIdAndDelete(albumID);
         return deleteAlbum;
     }
-
+    //Método UPDATE para modificar los datos de un album por su ID en mongodb
     async updateAlbum(albumID:string, createAlbumDTO:CreateAlbumDTO):Promise<Album>{
         const updateAlbum = await this.albumModel.findByIdAndUpdate(albumID, createAlbumDTO, {new:true});
         return updateAlbum;
