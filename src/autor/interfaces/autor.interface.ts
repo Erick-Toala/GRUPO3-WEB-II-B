de27f4178a99a2e6dd17f4c914ec311105a84da2
JOAN2021g <joangallardo1518@gmail.com>
2023-05-11 00:44:16 -0500

    update autor.interface.ts

--- src/autor/interfaces/autor.interface.ts
+++ src/autor/interfaces/autor.interface.ts
@@ -1,6 +1,11 @@
+//importamos el objeto Document para manejar la interface como un documento
 import {Document} from 'mongoose';
 
+
+//definimos una interface para detallar que es lo que vamos a estar manejando dentro del codigo
 export interface Autor extends Document{
+
+    //propiedades
     readonly nombre:string;
     readonly nacionalidad:string;
     readonly fechaNacimiento:Date;
