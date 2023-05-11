f51dadeac7a2116bd3f6470d7b000ee4b29790c4
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-05-10 22:04:33 -0500

    Update album.module.ts

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AlbumController } from './album.controller';
 import { AlbumService } from './album.service';
 
+//importamos el modulo de mongoose y el schema de la entidad album
 import { MongooseModule } from '@nestjs/mongoose';
 import { AlbumSchema } from './schemas/album.schema';
 
 @Module({
   imports:[
+    //definimos un nuevo Schema en mongoose para la entidad Album basada en el schema AlbumSchema
     MongooseModule.forFeature([
       {name:'Album', schema: AlbumSchema }
     ])
