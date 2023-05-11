82e4c9be756d7bfc4df7f3197af525d7d7a75fa1
JOAN2021g <joangallardo1518@gmail.com>
2023-05-11 00:52:16 -0500

    update autor.controller.ts

--- src/autor/autor.controller.ts
+++ src/autor/autor.controller.ts
@@ -15,6 +15,7 @@ export class AutorController {
     //instanciamos la clase AutorService en el constructor
     constructor(private autorService: AutorService){}
 
+     //creamos un ruta a traves del metodo POST que nos permita insertar nuevos autores
     @Post('/create')
     async createPost(@Res() res, @Body() createAutorDTO:CreateAutorDTO){
         const autor = await this.autorService.createAutor(createAutorDTO);
@@ -23,7 +24,7 @@ export class AutorController {
             autor
         })
     }
-
+//creamos un ruta a traves del metodo GET que nos permita obtener todos los autores
     @Get('/')
     async getAutores(@Res() res){
         const autores = await this.autorService.getAutores();
@@ -31,17 +32,21 @@ export class AutorController {
             autores
         })
     }
-
-    @ApiParam({
+  //creamos un ruta a traves del metodo GET que nos permita obtener un autor por su ID
+      @ApiParam({
         name:'autorID'
     })
     @Get('/:autorID')
     async getCancion(@Res() res, @Param('autorID') autorID){
         const autor = await this.autorService.getAutor(autorID);
-        if(!autor)throw new NotFoundException('El autor no existe');
+       
+        //si no recibo/encuentra ningun autor retornamos un error con el NotFoundException
+             if(!autor)throw new NotFoundException('El autor no existe');
         return res.status(HttpStatus.OK).json(autor);
     }
  
+    //creamos un ruta a traves del metodo DELETE que nos permita eliminar un autor por su ID
+   
     @ApiParam({
         name:'autorID'
     })
@@ -55,13 +60,16 @@ export class AutorController {
         });
     }
 
-    @ApiParam({
+     //creamos un ruta a traves del metodo PUT que nos permita actualizar un autor por su ID
+      @ApiParam({
         name:'autorID'
     })
     @Put('/update')
     async updateAutor(@Res() res, @Body() createAutorDTO:CreateAutorDTO, @Query('autorID')autorID){
         const actorUpdate = await this.autorService.updateAutor(autorID, createAutorDTO);
-        if(!actorUpdate)throw new NotFoundException('El autor no existe');
+       
+       //si no recibo/encuentra ningun autor retornamos un error con el NotFoundException
+              if(!actorUpdate)throw new NotFoundException('El autor no existe');
         return res.status(HttpStatus.OK).json({
             message:'Autor actualizada con éxito!',
             actorUpdate
