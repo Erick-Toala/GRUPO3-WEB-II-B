e3a2fba60eb0883ed4542bd7b702fb85c1cc8845
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-05-11 15:40:46 -0500

    Update cancion.interface.ts

--- src/cancion/interfaces/cancion.interface.ts
+++ src/cancion/interfaces/cancion.interface.ts
@@ -1,5 +1,10 @@
-import {Document} from 'mongoose';
+//importamos el objeto Document para manejar la interface como un documento
+import {Document} from 'mongoose'; 
+//definimos una interface para detallar que es lo que vamos a estar manejando dentro del codigo, 
+//no lo que se mueve en la aplicacion servidor-cliente
+//le decimos que esta interface hereda propiedades de Document
 export interface Cancion extends Document{
+ //definimos las propiedades
     readonly nombre:string;
     readonly duracion:string;
     readonly idArtista:string;
